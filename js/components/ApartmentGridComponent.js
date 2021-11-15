61bf6626bab98bfe0252a716221823f4e5bc5c0f
Titas Lukosius <titasgitara@gmail.com>
2021-11-15 19:02:08 +0200

    6: gauti API duomenys gridcomponente,

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -1,11 +1,39 @@
 class ApartmentGridComponent {
     constructor() {
-
+        this.state = {
+            loading: false,
+            apartments: []
+        }
         this.init();
     }
 
+    getApartments = () => API.fetchApartments(this.saveApartments, this.showError)
+
+    saveApartments = (apartments) => {
+        this.state.apartments = apartments;
+        this.state.loading = false;
+
+        this.render();
+    }
+
     init = () => {
+        this.state.loading = true;
+        this.getApartments();
+
+
         this.htmlElement = document.createElement('div');
         this.htmlElement.innerHTML = 'PaEjO';
+
+        this.render();
+    }
+
+    render = () => {
+        // console.log(this.state.apartments);
+        if(this.state.loading) {
+            this.htmlElement.innerHTML = 'siunčiama...';
+        } else {
+            this.htmlElement.innerHTML = 'parsiųsta!';
+
+        }
     }
 }
